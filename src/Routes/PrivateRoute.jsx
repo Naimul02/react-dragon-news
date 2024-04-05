e23a41cc1ae5f55844eee10e533edd4e08589464
Naimul02 <naimulislum39@gmail.com>
2024-04-05 22:46:37 +0600

    commit no 9

---
+++ src/Routes/PrivateRoute.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { useContext } from "react";
+import { Navigate, useLocation } from "react-router-dom";
+import { AuthContext } from "../Provider/AuthProvider";
+
+const PrivateRoute = ({ children }) => {
+  const { user, loading } = useContext(AuthContext);
+  const location = useLocation();
+  console.log(location);
+
+  if (loading) {
+    return <span className="loading loading-infinity loading-lg"></span>;
+  }
+  if (user) {
+    return children;
+  }
+  return <Navigate to="/login"></Navigate>;
+};
+
+export default PrivateRoute;
