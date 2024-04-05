b902ccad76c2e4601a8a232453423350e89c4b8a
Naimul02 <naimulislum39@gmail.com>
2024-04-05 15:51:52 +0600

    commit no 7

--- src/Pages/Reister/Register.jsx
+++ src/Pages/Reister/Register.jsx
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useContext } from "react";
 import Navbar from "../../Shared/Navbar/Navbar";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../../Provider/AuthProvider";
 const Register = () => {
+  const { createUser } = useContext(AuthContext);
   const handleRegister = (e) => {
     e.preventDefault();
+
+    const form = new FormData(e.currentTarget);
+    const name = form.get("name");
+    const photo = form.get("photo");
+    const email = form.get("email");
+    const password = form.get("password");
+    console.log(name, photo, email, password);
+    
+    //create user
+    createUser(email ,password)
+    .then(result => {
+      console.log(result.user)
+    })
+    .catch(error => {
+      console.error(error)
+    })
   };
   return (
     <div>
